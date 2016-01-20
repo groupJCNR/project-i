50551e46f309a5feb8ba0cce8d43e1f265e568f5
Relye <niklas.dahlin93@gmail.com>
2016-01-20 14:15:06 +0100

    User/WorkItem Entities
    variables & getters/setters

--- project-i/src/main/java/se/groupjcnr/projecti/model/User.java
+++ project-i/src/main/java/se/groupjcnr/projecti/model/User.java
@@ -1,5 +1,65 @@
 package se.groupjcnr.projecti.model;
 
-public class User {
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User extends AbstractEntity {
+
+	public enum Status {
+		ACTIVE, INACTIVE
+	}
+
+	private String firstName, lastName;
+	private List<Team> teamList;
+	private List<WorkItem> workList;
+	private Status status;
+
+	public User() {
+		super();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Team> getTeamList() {
+		return teamList;
+	}
+
+	public void setTeamList(List<Team> teamList) {
+		this.teamList = teamList;
+	}
+
+	public List<WorkItem> getWorkList() {
+		return workList;
+	}
+
+	public void setWorkList(List<WorkItem> workList) {
+		this.workList = workList;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 
 }
