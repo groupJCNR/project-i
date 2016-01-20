6c170d97b935ede957beb2528fbeb8149f9689fd
SuperSpaceNinja <phiphigt@gmail.com>
2016-01-20 12:00:28 +0100

    added dependencies

--- project-i/src/main/java/se/groupjcnr/projecti/model/AbstractEntity.java
+++ project-i/src/main/java/se/groupjcnr/projecti/model/AbstractEntity.java
@@ -1,5 +1,8 @@
 package se.groupjcnr.projecti.model;
 
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class AbstractEntity {
 
 }
--- project-i/src/main/java/se/groupjcnr/projecti/resource/ProjectiResource.java
+++ project-i/src/main/java/se/groupjcnr/projecti/resource/ProjectiResource.java
@@ -1,5 +1,8 @@
 package se.groupjcnr.projecti.resource;
 
+import javax.ws.rs.Path;
+
+@Path("project-i")
 public class ProjectiResource {
 
 }
