038bcddc86e8e4c1f0276fdc97556dfccfce8f1d
SuperSpaceNinja <phiphigt@gmail.com>
2016-01-20 14:15:21 +0100

    things

--- project-i/src/main/java/se/groupjcnr/projecti/model/AbstractEntity.java
+++ project-i/src/main/java/se/groupjcnr/projecti/model/AbstractEntity.java
@@ -1,8 +1,22 @@
 package se.groupjcnr.projecti.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	public AbstractEntity() {
+		super();
+	}
+	
+	public Long getId() {
+		return this.Id;
+	}
 }
--- project-i/src/main/java/se/groupjcnr/projecti/model/User.java
+++ project-i/src/main/java/se/groupjcnr/projecti/model/User.java
@@ -1,5 +1,45 @@
 package se.groupjcnr.projecti.model;
 
-public class User {
+import java.util.List;
 
+import javax.persistence.Entity;
+
+@Entity
+public class User extends AbstractEntity {
+
+	private String firstName;
+	private String lastName;
+	private List<Team> teams;
+	private List<WorkItem> workItems;
+	private boolean Active;
+	
+	protected User() {
+		super();
+	}
+	
+	public User(String firstName, String lastName) {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public List<Team> getTeams() {
+		return teams;
+	}
+
+	public List<WorkItem> getWorkItems() {
+		return workItems;
+	}
+
+	public boolean isActive() {
+		return Active;
+	}
+	
+	
 }
